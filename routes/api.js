e4096731574d093e40e332138688f0f60f31815b
Thomas Argent <tfargent@gmail.com>
2020-02-20 15:50:27 -0500

    trying to get routes to work. so far only bugs

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 const db = require("../models");
-const router = require("express").Router();
+
 
 module.exports = function(app){
-    router.get("/api/workouts", function(req,res){
+    app.get("/api/workouts", function(req,res){
         db.Workout.find({}).then(function(workouts){
             console.log(workouts)
             res.json(workouts)
